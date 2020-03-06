fff9ea9a38e0ad47f21bb6b0122263bef246c7a1
songning <songning@kedacom.com>
2020-03-06 15:37:07 +0800

    first commit

---
+++ src/main/java/com/sn/cykb/CykbServerApplication.java
@@ -0,0 +1,13 @@
+package com.sn.cykb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CykbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CykbServerApplication.class, args);
+    }
+
+}
