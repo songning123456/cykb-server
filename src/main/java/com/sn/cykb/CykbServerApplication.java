d0301c17f6905346ec7dadb0ca324b4e742677ec
songning <1457065857@qq.com>
2020-05-22 13:36:19 +0800

    spring cloud gateway + eureka

--- src/main/java/com/sn/cykb/CykbServerApplication.java
+++ src/main/java/com/sn/cykb/CykbServerApplication.java
@@ -2,11 +2,13 @@ package com.sn.cykb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author songning
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class CykbServerApplication {
 
     public static void main(String[] args) {
