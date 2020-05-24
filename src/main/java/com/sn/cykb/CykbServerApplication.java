b3229661c07394d35d71f751149ba0082ff9d701
songning123456 <1457065857@qq.com>
2020-05-24 11:29:46 +0800

    eureka 指定 ip

--- src/main/java/com/sn/cykb/CykbServerApplication.java
+++ src/main/java/com/sn/cykb/CykbServerApplication.java
@@ -2,13 +2,14 @@ package com.sn.cykb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author songning
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CykbServerApplication {
 
     public static void main(String[] args) {
