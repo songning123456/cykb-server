cfe5570603654c4727d445a1d1a4e750d9667953
songning123456 <1457065857@qq.com>
2020-05-24 14:23:27 +0800

    测试优化

--- src/main/java/com/sn/cykb/controller/HelloController.java
+++ src/main/java/com/sn/cykb/controller/HelloController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloController {
 
-    @Value("${test.name}")
+    @Value("${uni.name}")
     private String name;
 
     @GetMapping("/say")
