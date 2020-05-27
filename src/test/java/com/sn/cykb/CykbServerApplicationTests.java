6d4f54d013474ce95de245b61c5df1697adc3f55
songning123456 <1457065857@qq.com>
2020-05-27 21:18:57 +0800

    集成Spring Cloud Ribbon

--- src/test/java/com/sn/cykb/CykbServerApplicationTests.java
+++ src/test/java/com/sn/cykb/CykbServerApplicationTests.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 
 @Slf4j
@@ -16,13 +20,25 @@ public class CykbServerApplicationTests {
 
     @Autowired
     private GenerateIdDao generateIdDao;
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
 
     @Test
-    public void testDemo() {
+    public void testDubbo() {
         for (int i = 0; i < 100; i++) {
             long id = generateIdDao.generateId();
             log.info("id为: {}", id);
         }
+    }
+
+    @Test
+    public void testRibbon() {
+        this.loadBalancerClient.choose("uniId");
+        Map result = restTemplate.getForObject("http://uniId/uni-id/generate/id", Map.class);
+        assert result != null;
+        log.info("当前ribbon result: {}", result.toString());
     }
 
 }
