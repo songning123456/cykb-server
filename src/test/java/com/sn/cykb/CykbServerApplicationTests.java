0f4950773bce95c1e7b220daeba1634af804e42f
songning123456 <1457065857@qq.com>
2020-05-28 21:57:12 +0800

    暂时先生成10000个雪花id

--- src/test/java/com/sn/cykb/CykbServerApplicationTests.java
+++ src/test/java/com/sn/cykb/CykbServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.sn.cykb;
 
 import com.sn.cykb.rpcclient.GenerateIdDao;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -27,10 +34,13 @@ public class CykbServerApplicationTests {
 
     @Test
     public void testDubbo() {
-        for (int i = 0; i < 100; i++) {
+        List<Long> longList = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
             long id = generateIdDao.generateId();
+            longList.add(id);
             log.info("id为: {}", id);
         }
+        this.appendFile(longList);
     }
 
     @Test
@@ -39,6 +49,27 @@ public class CykbServerApplicationTests {
         Map result = restTemplate.getForObject("http://uniId/uni-id/generate/id", Map.class);
         assert result != null;
         log.info("当前ribbon result: {}", result.toString());
+    }
+
+    public void appendFile(List<Long> longList) {
+        try {
+            // 准备文件666.txt其中的内容是空的
+            File file = new File("D:/project/snowflake.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file, true);
+            // 把数据写入到输出流
+            for (long item : longList) {
+                fileWriter.write(Long.toString(item));
+                fileWriter.write("\r\n");
+            }
+            // 关闭输出流
+            fileWriter.close();
+            System.out.println("输入完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
